fffc7df21629f298adce2839c79ddf02d8554b40
DanielBergshoeff <dabergshoeff@gmail.com>
2019-09-19 17:10:03 +0200

    VFX stuff

--- Assets/Scripts/DanceOfLight.cs
+++ Assets/Scripts/DanceOfLight.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.VFX;
-using UnityEngine.VFX;
 
 public class DanceOfLight : MonoBehaviour
 {
